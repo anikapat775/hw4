a879c51c44ec90d93abbb9943381420739f699c8
Anika Patel <anikapat@usc.edu>
2022-10-18 00:01:33 +0000

    finished equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -3,11 +3,48 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int getHeight(Node * root)
+{
+    //base case
+    if(root == NULL){
+        return 0;
+    }
+    int leftHeight = 1 + getHeight(root->left);
+    int rightHeight = 1 + getHeight(root->right);
+    if(leftHeight > rightHeight){
+        return leftHeight;
+    }
+    else{
+        return rightHeight;
+    }
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    if(root == NULL){
+        return true;
+    }
+    
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+    int difference; 
+    if(leftHeight>rightHeight){
+        difference = leftHeight - rightHeight;
+    }
+    else{
+        difference = rightHeight - leftHeight;
+    }
+    if((root->left == NULL || root->right == NULL) && difference < 2){
+        return true;
+    }
+    if(leftHeight != rightHeight){
+        return false;
+    }
+    else{
+        bool leftEqual = equalPaths(root->left);
+        bool rightEqual = equalPaths(root->right);
+        return leftEqual && rightEqual;
+    }
 }
 
